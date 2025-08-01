fa70527765ef10a016e59d2d1d5a6f0fc2e9ff6d
celikhakan41 <celikhakan41@yahoo.com>
2025-08-01 11:32:26 +0300

    refactor: enhance BASE_URL configuration for API service

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,13 @@
 // src/services/api.js
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 
+  (import.meta.env.MODE === 'production' 
+    ? 'https://messaging-service-atux.onrender.com/api'
+    : 'http://localhost:8080/api');
 
 console.log("BASE_URL:", BASE_URL);
+console.log("MODE:", import.meta.env.MODE);
 
 export const register = (username, password) =>
   axios.post(`${BASE_URL}/auth/register`, { username, password });
